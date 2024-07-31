c991402b02d396ebcc71d43233cd73de08ab23f2
Gianluca Turra <gianlucaturra1@gmail.com>
2024-07-31 23:31:53 +0200

    Menu dropdown not working well

---
+++ derp/core/utils/apps.py
@@ -0,0 +1,33 @@
+"""Utility module for apps data
+"""
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class EndPoint:
+    """Class to carry data of single endpoints of an app
+    Each EndPoint its a single entry in the second menu opened
+    through the first one (installed apps)
+    """
+    name: str
+    url: str
+
+
+@dataclass
+class App:
+    """Class to carry data from installed app (modules) for menus
+    """
+    name: str
+    endpoints: List[EndPoint]
+
+
+installed_apps = [
+    App(
+        name='Items',
+        endpoints=[
+            EndPoint('Master File', '/items/master'),
+            EndPoint('Create Item', '/items/new'),
+        ]
+    )
+]
--- derp/core/views.py
+++ derp/core/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 
+from .utils.apps import installed_apps
+
 
 def index(request: HttpRequest) -> HttpResponse | HttpResponseRedirect:
     """
@@ -13,13 +15,18 @@ def index(request: HttpRequest) -> HttpResponse | HttpResponseRedirect:
     # TODO: insert index data
     return render(request, 'core/index.html')
 
-
 # Login view is not required since the default one it's good enough here
 
 @login_required
-def logout_user(request: HttpRequest):
+def logout_user(request: HttpRequest) -> HttpResponse:
     """Custom view for logout
-    Is this needed???
     """
     logout(request)
-    return index(request)  # TODO: This was poorly implemented, test it and document 
+    return index(request)
+
+
+@login_required
+def get_menu_apps(request: HttpRequest) -> HttpResponse:
+    """Return the menu for all apps
+    """
+    return render(request, 'core/menu.html', {'apps': installed_apps})
