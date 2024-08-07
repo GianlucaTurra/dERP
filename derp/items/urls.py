392d3214b54141db10781e63ed4b0086e16108c4
Gianluca Turra <gianlucaturra1@gmail.com>
2024-08-07 22:23:18 +0200

    modal_dialog refactoring and submit_cancel

--- derp/items/urls.py
+++ derp/items/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('master/', views.master_file, name='master'),
     path('delete/<uuid:uuid>', views.delete_item, name='delete'),
     path('update/inline/<uuid:uuid>', views.update_item_inline, name='update-inline'),
-    path('new/', views.new_item, name='new_item'),
+    path('new/', views.new_item, name='new'),
     path('new/inline', views.new_item_inline, name='new-inline'),
     path('detail/<uuid:uuid>', views.item_detail, name='detail'),
 ]
