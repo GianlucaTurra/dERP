95680913299e8eb607ed3a658ff686d2970e9db3
Gianluca Turra <gianlucaturra1@gmail.com>
2024-08-28 23:45:12 +0200

    Started stock units

--- derp/wearhouse/models.py
+++ derp/wearhouse/models.py
@@ -1,7 +1,9 @@
 # pylint: disable=missing-module-docstring,invalid-str-returned
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from django_extensions.db.models import TimeStampedModel
 from utils import abstract_models
+from items.models import Item
 
 
 class Wearhouse(
@@ -15,6 +17,11 @@ class Wearhouse(
     name = models.CharField(max_length=25, null=False, blank=False, unique=True)
     description = models.TextField(null=True)
     address = models.CharField(max_length=50, null=False, blank=False)
+    stockunits = models.ForeignKey(
+        'wearhouse.StockUnit',
+        verbose_name=_('Stock units'),
+        on_delete=models.CASCADE
+    )
 
     class Meta:
         """Meta information about the table
@@ -27,3 +34,28 @@ class Wearhouse(
 
     def __str__(self) -> str:
         return self.name
+
+
+class StockUnit(
+    TimeStampedModel,
+    abstract_models.UserOperations):
+    """Basic unit of a wearhouse
+    Each stockunit is capable of containing a certain amount of a 
+    specific item. It also carries information about the current state.
+    """
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    stored_quantity = models.FloatField(_('Stored quantity'), null=False)
+    max_quantity = models.FloatField(_('Maximum quantity'), null=False, blank=False)
+
+    class Meta:
+        """Meta information about the table
+        """
+        verbose_name_plural = 'Stock units'
+        db_table = 'stockunits'
+        get_latest_by = '-modified'
+
+    def __str__(self) -> str:
+        return f'{self.id}: {self.stored_quantity}/{self.max_quantity}'
+
+    def __repr__(self) -> str:
+        return self.id # type: ignore
